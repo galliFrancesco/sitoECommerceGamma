f3f236c1c02ffadf77f0983c11fb8a54e01328d3
galliFrancesco <galifrancesco.2809@gmail.com>
2022-05-04 19:06:08 +0200

    Aggiungi Prodotto al carrello

--- addItemSQL.php
+++ addItemSQL.php
@@ -1,4 +1,28 @@
 <?php
+session_start();
+include("connection.php");
+
+$IDSession = 1;
+
+if (isset($_SESSION["id"])) {
+    $IDSession = $_SESSION["id"];
+}
+
+// prende le cose dalla main page e lo mette nella tabella del carrello
+// magari passo le cose tramite session o query
+
+$IDA = $_GET['idArticolo']; // prende l'ID della casa passato tramite il link
+//echo $IDA; // gli passa il numero dell'articolo
+
+
+$sql = "INSERT INTO contiene (IDProdotto, IDCarrello) VALUES ($IDA, $IDSession)";
+
+if ($conn->query($sql) === TRUE) {
+    header("location:index.php");
+}
+
+
+/*
     session_start(); 
 
     include("connection.php"); 
@@ -16,7 +40,4 @@
     // ID*, IDProdotto, IDCarrello, Quantità
     // id carrello è da prelevare
     $sql = "INSERT INTO contiene (IDProdotto) VALUES ($idarticolo)"; 
-
-
-?>
-
+    */
